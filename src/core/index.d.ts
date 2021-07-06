6faac13e21cf69fb043cb838e26466006726b10b
hooksMan <1695415918@qq.com>
2021-07-06 12:37:10 +0800

    feat: edit .d.ts fileName

--- src/core/index.d.ts
+++ src/core/index.d.ts
@@ -1,13 +1,13 @@
 /*
  * @Author       : 邱狮杰
  * @Date         : 2021-07-05 12:38:26
- * @LastEditTime : 2021-07-05 12:40:19
+ * @LastEditTime : 2021-07-06 12:36:41
  * @FilePath     : /you-will-like/src/core/index.d.ts
  * @Description  : entry
  */
 
-/// <reference path="./chain/index.d.ts" />
-/// <reference path="./decorators/index.d.ts" />
-/// <reference path="./function/index.d.ts" />
-/// <reference path="./search/index.d.ts" />
-/// <reference path="./strategist/index.d.ts" />
+/// <reference path="./chain/chain.d.ts" />
+/// <reference path="./decorators/decorator.d.ts" />
+/// <reference path="./function/function.d.ts" />
+/// <reference path="./search/search.d.ts" />
+/// <reference path="./strategist/strategist.d.ts" />
