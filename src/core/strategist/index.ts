566811a34c06c45d8537729c1aeb97e00e91d8d1
Jsman <VuReact@qq.com>
2021-07-25 12:45:46 +0800

    feat: finish

--- src/core/strategist/index.ts
+++ src/core/strategist/index.ts
@@ -1,10 +1,9 @@
 /*
  * @Author: 邱狮杰
  * @Date: 2021-06-26 21:44:59
- * @LastEditTime: 2021-06-26 22:30:50
+ * @LastEditTime: 2021-07-25 12:45:00
  * @FilePath: /you-will-like/src/core/strategist/index.ts
  * @Description: 策略者
  */
 import { Strategist } from "./strategist";
-
 export { Strategist };
